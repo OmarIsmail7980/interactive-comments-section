7cf973ec4bb5dfbac3628ad944bacba9975f2d1c
Omar Ismail <omar.business7980@gmail.com>
2023-04-07 18:45:25 -0500

    add comment body

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -5,12 +5,12 @@ import LikeCounter from './LikeCounter'
 
 const Comment = () => {
   return (
-    <div className="bg-white flex justify-center p-4 rounded-md ">
+    <div className="bg-white flex p-4 rounded-md w-[35%] relative">
       <LikeCounter />
       <div className="flex flex-col ml-4">
         <CommentHeader />
+        <CommentBody />
       </div>
-      <CommentBody/>
     </div>
   );
 }
